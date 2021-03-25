0895d3c90cb1b1faaaa93d4fcd43d165f088a0d8
Bogdan Arizancu <bogdan@netzstrategen.com>
2021-03-25 18:19:28 +0100

    Fixed Gravity Forms should trigger cache flush for relevant pages or posts.

--- src/Plugin.php
+++ src/Plugin.php
@@ -53,6 +53,8 @@ class Plugin {
     add_action('switch_theme', __CLASS__ . '::purgeAll');
 
     add_action('shutdown', __CLASS__ . '::executePurge');
+
+    add_filter('gform_after_save_form', __CLASS__ . '::gform_after_save_form', 10, 2);
   }
 
   /**
@@ -211,6 +213,25 @@ class Plugin {
    */
   public static function getBasePath() {
     return dirname(__DIR__);
+  }
+
+  /**
+   * Hooks into Gravity Forms save for purging relevant posts cache.
+   *
+   * @implements gform_after_save_form
+   */
+  public static function gform_after_save_form($form, $is_new) {
+    global $wpdb;
+    $search_term = '%' . '[gravityform id="' . $form['id'] . '"%';
+    $target_pages = $wpdb->get_col("
+      SELECT p.ID FROM {$wpdb->posts} p
+      WHERE p.post_type IN ('page','post')
+        AND p.post_status = 'publish'
+        AND p.post_content LIKE '{$search_term}';
+    ");
+    foreach ($target_pages as $id) {
+      static::purgePost($id);
+    }
   }
 
 }
