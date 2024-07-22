23cfa8f95f565d46c6bec29593f5df8349848cc9
Daniel Kudwien <daniel@netzstrategen.com>
2024-07-22 12:42:05 +0200

    Fixed page of form is not purged when updating Gravityform. (#5)

--- src/Plugin.php
+++ src/Plugin.php
@@ -230,7 +230,7 @@ class Plugin {
     //   [gravityform id=\"48\"
     //   <!-- wp:html --> [gravityform id="48"
     //   <!-- wp:gravityforms/form {"formId":"48","formPreview":false}
-    $search_term = '([[]gravityform id=)(?:\\\\\\\\)?"' . $form['id'] . '(?:\\\\\\\\)?"';
+    $search_term = '[[]gravityform id=(\\\\\\\\)?"' . $form['id'] . '(\\\\\\\\)?"';
     $search_term .= '|wp\:gravityforms\/form {"formId":"' . $form['id'] . '"';
     $target_pages = $wpdb->get_col("
       SELECT p.ID FROM {$wpdb->posts} p
